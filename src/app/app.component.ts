171762c6c303549e79cb9e4e0f26bd3b510f19c4
Gedihared6409 <gediali019@gmail.com>
2020-04-04 09:08:42 +0300

    added the name of the auther as mine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import {Detail} from './detail'
 export class AppComponent {
 
   goals:Detail[] = [
-    new Detail(1,'\“You\’re off to great places, today is your day. Your mountain is waiting, so get on your way.\”','Dr. Seuss',"Martin",new Date(2020,2,4),0,0),
-    new Detail(2,'\“You always pass failure on the way to success.\”','Mickey Rooney', "Martin",new Date(2020,1,14),0,0),
-    new Detail(3,'\“Winning doesn’t always mean being first. Winning means you’re doing better than you’ve done before.\”' , 'Wolfgang Riebe', "Martin",new Date(2020,2,14),0,0),
-    new Detail(4,'“The only time you fail is when you fall down and stay down."' , 'Stephen Richards', "Martin",new Date(2020,2,14),0,0),
-    new Detail(5,'“Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.”', 'Lou Holtz', "Martin",new Date(2020,2,4),0,0),
-    new Detail(6,'“The way I see it, if you want the rainbow, you gotta put up with the rain.”', 'Dolly Parton', "Martin",new Date(2020,3,30),0,0),
+    new Detail(1,'\“You\’re off to great places, today is your day. Your mountain is waiting, so get on your way.\”','Dr. Seuss',"Ali",new Date(2020,2,4),0,0),
+    new Detail(2,'\“You always pass failure on the way to success.\”','Mickey Rooney', "Ali",new Date(2020,1,14),0,0),
+    new Detail(3,'\“Winning doesn’t always mean being first. Winning means you’re doing better than you’ve done before.\”' , 'Wolfgang Riebe', "Ali",new Date(2020,2,14),0,0),
+    new Detail(4,'“The only time you fail is when you fall down and stay down."' , 'Stephen Richards', "Ali",new Date(2020,2,14),0,0),
+    new Detail(5,'“Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.”', 'Lou Holtz', "Ali",new Date(2020,2,4),0,0),
+    new Detail(6,'“The way I see it, if you want the rainbow, you gotta put up with the rain.”', 'Dolly Parton', "Ali",new Date(2020,3,30),0,0),
   ];
   addNewQuote(quote){
     let quoteLength = this.goals.length;
